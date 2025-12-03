574bc436195ad01a831c5dc25783f91971464dc1
Luis Malave <unclelmm11@gmail.com>
2025-12-03 17:04:30 -0500

    actual screen functions

--- other_sudoku_stuff.py
+++
@@ -1,8 +0,0 @@
-while True:
-    start_screen()
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouseX, mouseY = event.pos
-        if rect_easy.collidepoint(mouseX, mouseY) or rect_medium.collidepoint(mouseX, mouseY) or rect_hard.collidepoint(mouseX, mouseY):
-
-    pygame.display.update()
